b907e0c96f721b5eb479c916963e8b3383fcbb9c
lmistie <romaniy11052001@gmail.com>
2022-03-21 23:14:44 +0300

    Add m for km

--- 1_Begginer/4_M-for-KM/translet-km.cpp
+++ 1_Begginer/4_M-for-KM/translet-km.cpp
@@ -1,15 +1,12 @@
 #include<iostream>
 using namespace std;
 
-
 int main()
 {
 setlocale(LC_ALL, "Rus");
 int m;
 float km;
-
 cout << "Перевод числа из метров в километры" << endl;
-
 cout << "Введите число в метрах:" << endl;
 cin>> m;
 km =(float)m / 1000;
